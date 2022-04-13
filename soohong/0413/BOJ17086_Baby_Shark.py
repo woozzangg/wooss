735fd71b585baefd4e266d7e72ea3cb827334b62
2riing <2riing2@gmail.com>
2022-04-13 21:57:58 +0900

    0413 update

--- soohong/0413/BOJ17086_Baby_Shark.py
+++ soohong/0413/BOJ17086_Baby_Shark.py
@@ -1,2 +1,37 @@
 import sys
 sys.stdin= open('input.txt')
+from collections import deque
+
+def BFS(point):
+    i, j = point
+    di, dj = [1, -1, 0, 0, 1, 1, -1, -1], [0, 0, 1, -1, 1, -1, -1, 1]
+    q = deque()
+    q.append((i, j))
+    shark_visited = [[0 for _ in range(M)] for _ in range(N)]
+    shark_visited[i][j] = 1
+    while q:
+        row, col = q.popleft()
+        for i in range(8):
+            ni, nj = di[i] + row, dj[i] + col
+            if 0<= ni < N and 0<= nj < M:
+                if shark_visited[ni][nj] == 0 and sharks[ni][nj] == 0:
+                    q.append((ni, nj))
+                    shark_visited[ni][nj] = shark_visited[row][col] + 1
+                elif sharks[ni][nj] == 1:
+                    result.append(shark_visited[row][col])
+                    return
+
+
+N, M = map(int, input().split())
+sharks = [list(map(int, input().split())) for _ in range(N)]
+visited = [ [0 for _ in range(M)] for _ in range(N)]
+result = []
+
+
+for i in range(N):
+    for j in range(M):
+        if visited[i][j] == 0 and sharks[i][j] == 0:
+            BFS((i, j))
+            visited[i][j] = 1
+
+print(max(result))
---
+++ soohong/0413/BOJ2108_Tong.py
@@ -0,0 +1,47 @@
+import sys
+sys.stdin= open('input.txt')
+
+
+def fre():
+    num = list(set(arr))
+    num.sort()
+    cnt = [0] * len(num)
+
+    for a in arr:
+        for n in range(len(num)):
+            if a == num[n]:
+                cnt[n] += 1
+    max_cnt = max(cnt)
+    index_cnt = 1
+    if cnt.count(max_cnt) > 1:  # 여러개면
+        for i in range(len(cnt)):
+            if index_cnt == 2:
+                print(num[i])
+                return
+            elif cnt[i] == max_cnt:
+                index_cnt += 1
+            else:
+                pass
+    else:
+        for i in range(len(cnt)):
+            if cnt[i] == max_cnt:
+                print(num[i])
+                return
+
+
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(int(input()))
+# 산술평균
+print(int(round(sum(arr)/N,0)))
+
+# 중앙값 n//2 번째 값
+arr.sort()
+print(arr[N//2])
+
+# 최빈값
+fre()
+
+# 범위
+print(max(arr)- min(arr))
