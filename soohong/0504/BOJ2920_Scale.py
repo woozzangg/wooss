2028bf6daba428b698f3d8c85be166834134ce7d
2riing <2riing2@gamil.com>
2022-04-29 15:59:21 +0900

    2920, 11403

---
+++ soohong/0504/BOJ2920_Scale.py
@@ -0,0 +1,44 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+# 먼저 첫번째와 두번째를 비교해서
+# 1 < 2 어센딩
+# 1 > 2 디센딩
+# 그 이후값이 어센딩 or 디센딩 조건 만족못하면 믹스드
+
+numbers = list(map(int, input().split()))
+
+def find_scale():
+    N = len(numbers)
+    scale = ['ascending',
+             'descending',
+             'mixed']
+    result = ''
+    if numbers[0] < numbers[1]:
+        result = scale[0]
+    elif numbers[0] > numbers[1]:
+        result = scale[1]
+    else:
+        result = scale[2]
+        return result
+
+    for i in range(1, N-1):
+        if result == scale[0] and numbers[i] < numbers[i+1]:
+            pass
+        elif result == scale[1] and numbers[i] > numbers[i+1]:
+            pass
+        else:
+            result = scale[2]
+            return result
+    return result
+
+print(find_scale())
+
+
+
+
+
+
+
+
