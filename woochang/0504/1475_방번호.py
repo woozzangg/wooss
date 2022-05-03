0299a52514c036312c0c55927be179240cd5a74a
Woozzang <dannba1@naver.com>
2022-05-03 23:44:31 +0900

    0503 algo wc

---
+++ woochang/0504/1475_방번호.py
@@ -0,0 +1,20 @@
+
+
+
+N = str(input())
+num = []
+for i in range(len(N)):
+    num.append(int(N[i]))
+# num.sort()
+print(num)
+# cnt = 0
+# for i in num:
+#     if i == 6 or i == 9:
+#         cnt += 1
+# print((cnt // 2) + 1)
+
+cnt = int(N)
+a = num.pop()
+print(a)
+
+
