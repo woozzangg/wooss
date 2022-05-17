257297c7be5b1b1e35f1d96b9080c739d5b68e41
Woozzang <dannba1@naver.com>
2022-05-18 00:43:44 +0900

    0518 algo wc

---
+++ woochang/0518/10816_숫자카드2.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('10816input.txt')
+
+N = int(input())
+N_list = list(map(int ,input().split()))
+M = int(input())
+M_list = list(map(int ,input().split()))
+
+# for i in M_list:
+#     cnt = 0
+#     for j in N_list:
+#         if j == i:
+#             cnt += 1
+#     print(cnt, end=' ')
+
+M_index = [0]*M
+for i in range(len(M_list)):
+    M_index[i] = N_list.count(M_list[i])
+print(M_index)
