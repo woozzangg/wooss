500b195ba244741f033bdedb37f7eb0e7a8fecc3
2riing <2riing2@gmail.com>
2022-06-22 14:32:49 +0900

    0622 sh

--- Algorithm/0622/[BOJ_9204]체스/염수홍.py
+++ Algorithm/0622/[BOJ_9204]체스/염수홍.py
@@ -2,11 +2,31 @@ import sys
 sys.stdin = open('input1.txt')
 from collections import deque
 
-words = {'A': 1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8}
+words = {'A': 0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+nums = {'1':7 , '2':6, '3':5, '4':4, '5':3, '6':2, '7':1, '8':0}
+
+answer = deque()
+def FindRoute():
+    global results
+    for result_idx in range(len(results)):
+        if result_idx[0] == end[0] and result_idx[1] == end[1]:
+            parent = (result_idx[2], result_idx[3])
+            answer.append(parent)
+    while True:
+        if parent == start:
+            return
+        else:
+            for result_idx in range(len(results)):
+                if result_idx[0] == parent[0] and result_idx[1] == parent[1]:
+                    parent = (result_idx[2], result_idx[3])
+                    answer.appendleft(parent)
+                    print(parent)
 
 def BFS(point):
+    global result
     q = deque()
     q.append(point)
+    visited[point[0]][point[1]] = 1
     di, dj = [1, -1, 1, -1 ], [1, -1, -1, 1]
     cnt = 0
     while q:
@@ -18,8 +38,12 @@ def BFS(point):
                 ni, nj = row + di[i], col + dj[i]
                 if 0 <= ni < 8 and 0 <= nj < 8:
                     if visited[ni][nj] == 0:
+
+                        results.append([ni, nj, row, col])
+                        print([ni, nj, row, col])
                         q.append((ni, nj))
                         visited[ni][nj] = visited[row][col] + 1
+
     return 'Impossible'
 
 
@@ -28,8 +52,9 @@ T = int(input())
 for tc in range(1, T+1):
     visited = [[0] * 8 for _ in range(8)]
     a, b, c, d = map(str, input().split())
-    start, end = (words[a]-1, int(b)-1), (words[c]-1, int(d)-1)
-
+    start, end = (nums[b], words[a]), (nums[d], words[c])
+    print(start, end)
+    results = []
     print(BFS(start))
-
     print(visited)
+    print(start, answer)
