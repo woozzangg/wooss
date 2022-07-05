369c21f8e8360a0f3fb58295765b2beeca6c13c2
Woozzang <dannba1@naver.com>
2022-07-05 21:31:25 +0900

    0705 algo wc

--- Algorithm/0706/[BOJ_14235]크리스마스 선물/우창.py
+++ Algorithm/0706/[BOJ_14235]크리스마스 선물/우창.py
@@ -1,19 +1,36 @@
 import sys
 sys.stdin = open('input1.txt')
 # input = sys.stdin.readline
+import heapq
 
-N = int(input())
-arr = []
+# N = int(input())
+# arr = []
+#
+# for i in range(N):
+#     pr = input()
+#     if pr != '0 ':
+#         pr = list(map(int, pr.split()))
+#         for j in range(pr[0]):
+#             arr.append(pr[j+1])
+#     else:
+#         if arr:
+#             print(arr[0])
+#             arr.pop(0)
+#         else:
+#             print('-1')
 
-for i in range(N):
+n = int(input())
+li = []
+
+for i in range(n):
     pr = input()
-    if pr != '0 ':
-        pr = list(map(int, pr.split()))
-        for j in range(pr[0]):
-            arr.append(pr[j+1])
-    else:
-        if arr:
-            print(arr[0])
-            arr.pop(0)
+    if pr == '0 ':
+        if li:
+            ans = -heapq.heappop(li) # 최솟값 pop
+            print(ans)
         else:
             print('-1')
+    else:
+        arr = list(map(int, pr.split()))
+        for j in range(arr[0]):
+            heapq.heappush(li, -arr[j+1]) #heappush(리스트, 범위)
