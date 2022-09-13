e46008c1490ec1f51efba3ab3283c16745c902a5
Woozzang <dannba1@naver.com>
2022-09-14 03:06:08 +0900

    [PRO] 키패드-누르기 레벨1 32min

--- Algorithm/0914/[PRO]키패드-누르기/김우창.py
+++ Algorithm/0914/[PRO]키패드-누르기/김우창.py
@@ -1,7 +1,6 @@
 numbers = [[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],[7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2],[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]]
 hand =["right","left","right"]
 
-
 def solution(numbers, hand):
     answer = ''
     for i in range(len(numbers)):
@@ -46,6 +45,7 @@ def solution(numbers, hand):
                     L_position = i
 
     return answer
+
 
 for i in range(len(numbers)):
     solution(numbers[i], hand[i])
